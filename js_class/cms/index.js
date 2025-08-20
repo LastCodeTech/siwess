5481fd01d8ffb41ce69ab1e09a13edb7415f17bb
= <sulaimonabubakre260@gmail.com>
2025-08-20 10:29:45 +0100

    working with funtion

--- js_class/cms/index.js
+++ js_class/cms/index.js
@@ -7,10 +7,32 @@
 // })
 
 let chooser = document.getElementById('chooser');
+let space = document.getElementById('space');
 chooser.addEventListener('change', function(){
-document.body.style.backgroundColor = this.value;
+space.style.backgroundColor = this.value;
 })//mine
 let lister = document.getElementById('lister');
 lister.addEventListener('change', function(){
-document.body.style.color = this.value;
+space.style.color = this.value;
 })
+
+// Font sizer
+let font_size = document.getElementById('font_size');
+let texts = document.getElementById('texts');
+
+
+font_size.addEventListener('change', function(){
+
+    space.style.fontSize = this.value+"rem";
+
+})
+let b = 'biscuit';
+let x = 'sweet';
+//let y = 'the + " " + b + " " + is  + " " + x';
+let z = 'the ' + b +' is ' + x;
+console.log(z);
+let m = 2;
+let n='10';
+console.log(m+n);
+
+
